f9e8257976802c87191b3d1e20bca457876074c1
tsubai <tatsuya7514@gmail.com>
2017-11-02 20:14:35 +0900

    東統合。吹き出し用数値実装

---
+++ プログラム/椿井/2017年11月2日東結合/Assets/script/SuutiScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SuutiScript : MonoBehaviour {
+
+    private player_script player;
+    public GameObject target;
+    private int suuti;
+    public Text Suutitext;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+   
+        if (target.gameObject.tag == "Player")
+        {
+            player = GameObject.Find(target.gameObject.transform.name).GetComponent<player_script>();
+            //  HP -= player.PlayerATK;//攻撃で体力が減少
+        suuti = player.Number;
+        Suutitext.text = ""+suuti ;
+    }
+
+
+    }
+
+}
