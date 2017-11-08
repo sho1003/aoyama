340f237ce4bf4ae1663fff61d3a624afab1b471a
arukino <keiskue.soccer18@gmail.com>
2017-11-08 14:33:52 +0900

    シーン遷移の修正と簡易な勝敗判定の追加

--- プログラム/2.結合＿Data/11.07/一井結合/Assets/script/TimeScript.cs
+++ プログラム/2.結合＿Data/11.07/一井結合/Assets/script/TimeScript.cs
@@ -1,20 +1,29 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TimeScript : MonoBehaviour
 {
+    public enum Winner
+    {
+        RED,
+        BLUE,
+        DROW
+    }
 
     public float time;
     public Text Timetext;
 
+    private int JudgeFlag;
+    private ScoreScript score;
+
     // Use this for initiailzation
     void Start()
     {
-
-
+        JudgeFlag = 0;
         //time = 10.0f;
-
+        score = GameObject.Find("Score").GetComponent<ScoreScript>();
     }
 
     // Update is called once per frame
@@ -27,11 +36,22 @@ public class TimeScript : MonoBehaviour
         if (time <= 0)
         {
 
-            Application.LoadLevel("Result");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
             time = 0;
             //↓リザルト判定
+            if (score.RedScore > score.BlueScore)
+                //　赤の勝利
+                JudgeFlag = (int)Winner.RED;
+            else if (score.RedScore < score.BlueScore)
+                //　青の勝利
+                JudgeFlag = (int)Winner.BLUE;
+            else
+                //　引き分け
+                JudgeFlag = (int)Winner.DROW;
 
+            PlayerPrefs.SetInt("JudgeFlag", JudgeFlag);
         }
 
     }
+
 }
---
+++ プログラム/2.結合＿Data/2017年11月2日東結合/Assets/script/Result.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Result : MonoBehaviour {
+
+    public Text text;
+
+    // Use this for initialization
+    void Start()
+    {
+        if(PlayerPrefs.GetInt("JudgeFlag")==
+            (int)TimeScript.Winner.RED)
+        {
+            text.text = "赤の勝利！！";
+            text.color = Color.red;
+        }
+        else if(PlayerPrefs.GetInt("JudgeFlag")==
+            (int)TimeScript.Winner.BLUE)
+        {
+            text.text = "青の勝利！！";
+            text.color = Color.blue;
+        }
+        else
+        {
+            text.text = "引き分け！！";
+            text.color = Color.black;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Title");
+        }	
+    }
+
+}
